eb662bf95a1a260f1daa45ca2b7ab860d7ceec63
Neo Rosenlund <Neo.Rosenlund@elev.ga.ntig.se>
2024-10-14 10:56:17 +0200

    jumping tests

--- Assets/Scripts/Characters/Movement.cs
+++ Assets/Scripts/Characters/Movement.cs
@@ -13,7 +13,9 @@ public class Movement : MonoBehaviour
     public Vector3 MoveDirection;
     [SerializeField] private float MoveSpeed = 5;
     [SerializeField] private float JumpForce = 10;
-    private int jumpCount = 1;
+    [SerializeField] private int jumpCount = 0;
+    [SerializeField] string GroundTag;
+    public float gravity;
 
     private void OnEnable() {
         Controlls.Enable();    
@@ -26,20 +28,26 @@ public class Movement : MonoBehaviour
         MoveDirection = Controlls.ReadValue<Vector3>();
     }
     private void FixedUpdate() {
-        rb.AddForce(0, -200, 0);
+        rb.AddForce(0, -gravity, 0);
         rb.velocity = new Vector3(0, 0, MoveDirection.x*MoveSpeed); 
 
         if(jumpCount > 0){
-        rb.AddForce(new Vector3(0,MoveDirection.y * JumpForce,0), ForceMode.Impulse);
-        jumpCount-=1;
+        rb.AddForce(new Vector3(0,MoveDirection.y * JumpForce,0));
         }
     }
 
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter(Collider other) {
+    if(other.tag == GroundTag){
         if(jumpCount == 0){
             jumpCount += 1;
         }
     }
-
+    }
+    private void OnTriggerExit(Collider other) {
+       if(other.tag == GroundTag){
+        jumpCount = 0;
+    } 
+    }
 }
+
