e59776d83a92faec831754f55cbd5a1e86d48e66
Neo Rosenlund <Neo.Rosenlund@elev.ga.ntig.se>
2024-10-16 16:13:00 +0200

    p1 of adding items

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumptest : MonoBehaviour
+{
+    public float JumpForce;
+    public float GroundDistance = 0.5f;
+
+    public float MoveSpeed;
+    float MoveDirection;
+
+    public Rigidbody rb;
+
+    bool IsGrounded(){
+        return Physics.Raycast(transform.position, Vector3.down, GroundDistance);
+    }
+
+    private void Update() {
+
+        
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true){
+            rb.velocity = new Vector3(rb.velocity.x,1,rb.velocity.z) * JumpForce;
+        }
+
+        float MoveDirection = Input.GetAxisRaw("Horizontal");
+
+
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, MoveDirection*MoveSpeed);
+
+    }
+}
